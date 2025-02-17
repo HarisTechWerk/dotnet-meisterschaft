fd2d446a9ad81e27d7efd9e0f2dd9ac59ff272f8
Haris Qureshi <92890149+HarisTechWerk@users.noreply.github.com>
2025-02-17 23:27:45 +0100

    Implemented Dependency Injection using NotificationManager

---
+++ csharp-profiweg/NotificationServiceDemo/src/NotificationManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationServiceDemo.src
+{
+    public class NotificationManager
+    {
+        private readonly INotificationService _notificationService;
+
+        public NotificationManager(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        public void NotifyUser(string message, string recipient)
+        {
+            _notificationService.SendNotification(message, recipient);
+        }
+    }
+}
--- csharp-profiweg/NotificationServiceDemo/src/Program.cs
+++ csharp-profiweg/NotificationServiceDemo/src/Program.cs
@@ -6,16 +6,20 @@ namespace NotificationServiceDemo.src
     {
         static void Main(string[] args)
         {
-            INotificationService emailService = new EmailNotificationService();
+            // Inject different notification services dynamically
 
+            // Email Notification
+            var emailManager = new NotificationManager(new EmailNotificationService());
+            emailManager.NotifyUser("Welcome to our Service!", "user@email.de");
 
-            emailService.SendNotification("Welcome to our Service!", "user@email.com");
+            // SMS Notification
+            var smsManager = new NotificationManager(new SMSNotificationService());
+            smsManager.NotifyUser("Welcome to our Service!", "+49123456789");
 
-            INotificationService smsService = new SMSNotificationService();
-            smsService.SendNotification("Welcome to our Service!", "+49123456789");
+            // Push Notification
+            var pushManager = new NotificationManager(new PushNotificationService());
+            pushManager.NotifyUser("Welcome to our Service!", "user_device_token");
 
-            INotificationService pushService = new PushNotificationService();
-            pushService.SendNotification("Welcome to our Service", "user_device_token");
         }
     }
 }
