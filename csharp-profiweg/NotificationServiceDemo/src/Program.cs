7080192d239d818eae27cec3b1b04c31eecd7627
Haris Qureshi <92890149+HarisTechWerk@users.noreply.github.com>
2025-02-17 23:12:38 +0100

    moved Program.cs inside for better structure

---
+++ csharp-profiweg/NotificationServiceDemo/src/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NotificationServiceDemo.src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            INotificationService emailService = new EmailNotificationService();
+
+            emailService.SendNotification("Welcome to our Service!", "user@email.com");
+        }
+    }
+}
